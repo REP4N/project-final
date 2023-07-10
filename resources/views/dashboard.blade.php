6d6bbc09898b74c55c77af139876fab6a97c195d
REP4N <201220041repan@gmail.com>
2023-07-10 15:12:44 +0700

    updet asset

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('components.layout')
+{{-- @extends('components.layout')
 
 @section('content')
 <div class="container">
@@ -20,4 +20,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
